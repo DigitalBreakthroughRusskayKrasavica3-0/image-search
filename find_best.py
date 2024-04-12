e4c01bacf61430df38a04d709a2b57a6baa279c7
Sam Komarov <sam@skomarov.com>
2024-04-13 02:43:32 +0300

    sentence-transformers/clip-ViT-B-32-multilingual-v1

--- find_best.py
+++ find_best.py
@@ -1,4 +1,5 @@
 import pandas 
+import json
 import sys
 from PIL import Image
 import clip
@@ -10,30 +11,40 @@ df = df[:30]
 df['path'] = './mincult-train/train/'+ df['object_id'].astype(str) + '/' + df['img_name']
 df['embedding_path'] = df['path']+'.embedding'
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"using {device}")
-device = torch.device(device)
-model, preprocess = clip.load("ViT-L/14")
-model.to(device)
+from sentence_transformers import SentenceTransformer, util
 
-def get_embedding(image_path): 
-    image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
-    with torch.no_grad():
-        return model.encode_image(image)[0].tolist()
+# We use the original clip-ViT-B-32 for encoding images
+img_model = SentenceTransformer('clip-ViT-B-32')
 
-def find_best(image_path): 
-    emb = get_embedding(image_path) 
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# print(f"using {device}")
+# device = torch.device(device)
+# model, preprocess = clip.load("ViT-L/14")
+# model.to(device)
+
+# def get_embedding(img): 
+#     if isinstance(img, str):
+#         img = Image.open(img)
+#     image = preprocess(img).unsqueeze(0).to(device)
+#     with torch.no_grad():
+#         return model.encode_image(image)[0].tolist()
+
+def find_best(img): 
+    if isinstance(img, str): 
+        img = Image.open(img)
+    emb = img_model.encode([img])[0].tolist()
     distances = {} 
     for index, row in df.iterrows(): 
         with open(row.embedding_path, 'r') as f:
-            other_emb = eval(f.readline()) # TODO: parse correctly
+            other_emb = json.loads(f.readline()) 
             dist = distance.cosine(emb, other_emb)
             distances[index] = dist
     dists = sorted(list(distances.items()), key=lambda a: a[1])[:10] 
     res = []
     for i, dist in dists: 
-        res.append((df.iloc[i].path, dist))
-    return res
+        res.append((df.iloc[i].path))#, dist))
+    print(res)
+    return res[0], res[1:]
 
 if __name__ == '__main__': 
     print(find_best(sys.argv[1]))
--- get_categories.py
+++ get_categories.py
@@ -1,23 +1,36 @@
 import csv
+import sys
+from scipy.spatial import distance
 import clip
 import torch
 from PIL import Image
-import translators as ts
+# import translators as ts
 
-from find_best import model, device, preprocess
+# from find_best import model, device, preprocess
+
+from sentence_transformers import SentenceTransformer, util
+from find_best import img_model
+
+# We use the original clip-ViT-B-32 for encoding images
+# img_model = SentenceTransformer('clip-ViT-B-32')
+
+# Our text embedding model is aligned to the img_model and maps 50+
+# languages to the same vector space
+text_model = SentenceTransformer('sentence-transformers/clip-ViT-B-32-multilingual-v1')
+
+
 
 data_path = './mincult-train/train.csv'
-
 # categories parsing
 categories = []
-translations = {}
+# translations = {}
 with open(data_path, encoding='utf-8', newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
     for row in reader:
-        if row['group'] not in translations.values():
-            category = ts.translate_text(row['group'], translator='bing', from_language='ru')
-            categories.append(category)
-            translations[category] = row['group']
+        if row['group'] not in categories:
+            # category = ts.translate_text(row['group'], translator='bing', from_language='ru')
+            categories.append(row['group'])
+            # translations[category] = row['group']
 
 # ЛОЖКУ ОПРЕДЕЛЯЕТ КАК ОРУЖИЕ
 
@@ -30,20 +43,32 @@ def get_categories(img: str | Image.Image):
 
 
     # Vectors creating
-    image_input = preprocess(img).unsqueeze(0).to(device)
-    text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in categories]).to(device)
+    # image_input = preprocess(img).unsqueeze(0).to(device)
+    # text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in categories]).to(device)
 
-    # Calculate features
-    with torch.no_grad():
-        image_features = model.encode_image(image_input)
-        text_features = model.encode_text(text_inputs)
+    img_embedding = img_model.encode([img])[0]
+    texts = [f"это музейный экспонат категории {c}" for c in categories]
+
+    text_embeddings = text_model.encode(texts)
+    dists = {} 
+    for i, t in enumerate(texts): 
+        dists[t] = distance.cosine(text_embeddings[i], img_embedding)
+    print(sorted(list(dists.items()),key=lambda a: a[1])[:5])
+
+    # for text, scores in zip(texts, cos_sim):
+    #     max_img_idx = torch.argmax(scores)
+    #     print("Text:", text)
+    #     print("Score:", scores[max_img_idx] )
 
     # Pick the top 5 most similar labels for the image
-    image_features /= image_features.norm(dim=-1, keepdim=True)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
-    similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-    values, indices = similarity[0].topk(15)
+    # image_features /= image_features.norm(dim=-1, keepdim=True)
+    # text_features /= text_features.norm(dim=-1, keepdim=True)
+    # similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+    # values, indices = similarity[0].topk(15)
 
-    res = [(translations[categories[index]], 100 * value.item()) for value, index in zip(values, indices)]
-    print(res) 
+    # res = [(categories[index], 100 * value.item()) for value, index in zip(values, indices)]
+    # print(res) 
     return
+
+if __name__ == '__main__': 
+    get_categories(sys.argv[1])
